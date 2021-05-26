6cf7d655e5edc6620edfa2df220f6964d843a853
dventuri <diegoventuri@gmail.com>
2021-05-26 13:55:22 -0300

    comments added

--- profile_MFSim_bilinear_2.py
+++ profile_MFSim_bilinear_2.py
@@ -118,19 +118,17 @@ if __name__ == '__main__':
 
     vertices[:,0] -= 3.464  # center x values at 0
 
-    # value = interpolate_value(0.5, -0.2, vertices, vertices_connect)
-    # print(value)
-
-    # Define funciton to create MFSim-like grid
+    # create MFSim-like grid (vertices)
     space = np.arange(-0.5,0.5001,0.025)
-    # space = np.arange(-0.5,0.5001,0.003125)
     X,Y = np.meshgrid(space, space)
 
+    # define centroids
     space_centroid = np.empty(len(space)-1)
     for i in range(1,len(space)):
         space_centroid[i-1] = space[i-1] + (space[i]-space[i-1])/2
     X_c,Y_c = np.meshgrid(space_centroid, space_centroid)
 
+    # interpolate
     percent = 0
     n = 0
     Z = np.empty((len(space_centroid),len(space_centroid)))
@@ -142,6 +140,7 @@ if __name__ == '__main__':
             percent = n/total*100
             print(percent,"%")
 
+    # plot
     circle = plt.Circle((0, 0), 0.33, color='white', fill=False, lw=3)
     fig, ax = plt.subplots(figsize=(7,6))
     c = ax.pcolor(X, Y, -Z,
