d52f28f9383df98aefc8c89a4dc9cc937543ae56
dventuri <diegoventuri@gmail.com>
2021-06-24 21:25:55 -0300

    simplified functions and set color limits on plots

--- swirl_profile.py
+++ swirl_profile.py
@@ -1,18 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 
-def func_R(y,z):
-    return np.sqrt(z**2 + y**2)
+def func_R(y, z):
+    return np.sqrt(y**2 + z**2)
 
 def y_vel_MFSim(y, z, aux=None):
     if(not aux):
         aux = func_R(y,z)
-    return aux*2*np.pi*150000*(z*np.sin(np.pi/2)+y*np.cos(np.pi/2))
+    return aux*2*np.pi*150000*z
 
 def z_vel_MFSim(y, z, aux=None):
     if(not aux):
         aux = func_R(y,z)
-    return aux*2*np.pi*150000*(-y*np.sin(np.pi/2)+z*np.cos(np.pi/2))
+    return aux*2*np.pi*150000*(-y)
 
 def y_vel(y, z, aux=None):
     if(not aux):
@@ -27,11 +28,10 @@ def z_vel(y, z, aux=None):
 
 r_jet = 0.0055
 
-start = 0.5-r_jet
-stop = 0.5+r_jet
+start = -r_jet
+stop = r_jet
 
 space = np.linspace(start,stop,50)
-space = space-0.5
 yy, zz = np.meshgrid(space, space)
 
 R = func_R(yy, zz)
@@ -53,11 +53,15 @@ fig, ax = plt.subplots()
 plot = ax.contourf(yy, zz, vel_z_MFSim)
 fig.colorbar(plot)
 
+max_vel = y_vel_MFSim(0,0.0055)
+vel_mag = np.sqrt(vel_y_MFSim**2 + vel_z_MFSim**2)
 fig, ax = plt.subplots()
 strm = ax.streamplot(yy, zz, vel_y_MFSim, vel_z_MFSim,
                     density=1,
-                    color=np.sqrt(vel_y_MFSim**2 + vel_z_MFSim**2),
-                    cmap='viridis'
+                    color=vel_mag,
+                    cmap='viridis',
+                    norm=colors.Normalize(vmin=0,
+                                          vmax=max_vel)
                     )
 ax.set_xlim(-r_jet,+r_jet)
 ax.set_ylim(-r_jet,+r_jet)
@@ -97,11 +101,15 @@ fig, ax = plt.subplots()
 plot = ax.contourf(yy, zz, vel_z)
 fig.colorbar(plot)
 
+max_vel = np.abs(y_vel(0,0.0055))
+vel_mag = np.sqrt(vel_y**2 + vel_z**2)
 fig, ax = plt.subplots()
 strm = ax.streamplot(yy, zz, vel_y, vel_z,
                     density=1,
-                    color=np.sqrt(vel_y**2 + vel_z**2),
-                    cmap='viridis'
+                    color=vel_mag,
+                    cmap='viridis',
+                    norm=colors.Normalize(vmin=0,
+                                          vmax=max_vel)
                     )
 ax.set_xlim(-r_jet,+r_jet)
 ax.set_ylim(-r_jet,+r_jet)
