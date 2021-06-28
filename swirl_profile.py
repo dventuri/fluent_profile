5a202c7d4b17d04ff4a7b57744c4c1eac8481cf3
dventuri <diegoventuri@gmail.com>
2021-06-28 14:40:29 -0300

    more vorticity plots

--- swirl_profile.py
+++ swirl_profile.py
@@ -5,26 +5,31 @@ import matplotlib.colors as colors
 def func_R(y, z):
     return np.sqrt(y**2 + z**2)
 
-def y_vel_MFSim(y, z, aux=None):
+def y_vel_MFSim(y, z, C=150000, aux=None):
     if(not aux):
         aux = func_R(y,z)
-    return aux*2*np.pi*150000*z
+    return aux*2*np.pi*C*z
 
-def z_vel_MFSim(y, z, aux=None):
+def z_vel_MFSim(y, z, C=150000, aux=None):
     if(not aux):
         aux = func_R(y,z)
-    return aux*2*np.pi*150000*(-y)
+    return aux*2*np.pi*C*(-y)
 
-def y_vel(y, z, aux=None):
+def y_vel(y, z, C=150000, aux=None):
     if(not aux):
         aux = func_R(y,z)
-    return -aux*150000*2*np.sin(np.pi*z)*np.cos(np.pi*y)
+    return aux*C*2*np.sin(np.pi*z)*np.cos(np.pi*y)
+        #  aux*C*2*np.pi*z
 
-def z_vel(y, z, aux=None):
+def z_vel(y, z, C=150000, aux=None):
     if(not aux):
         aux = func_R(y,z)
-    return aux*150000*2*np.sin(np.pi*y)*np.cos(np.pi*z)
+    return -aux*C*2*np.sin(np.pi*y)*np.cos(np.pi*z)
+        #  -aux*C*2*np.pi*y
 
+# Small angle approximation
+# sin(theta) = theta
+# cos(theta) = 1 - theta^2/2 = 1
 
 r_jet = 0.0055
 
@@ -42,21 +47,21 @@ plot = ax.contourf(yy, zz, R)
 fig.colorbar(plot)
 
 # MFSim - y and z velocities with aux function
-vel_y_MFSim = y_vel_MFSim(yy,zz)
-vel_z_MFSim = z_vel_MFSim(yy,zz)
+vel_y = y_vel_MFSim(yy,zz)
+vel_z = z_vel_MFSim(yy,zz)
 
 fig, ax = plt.subplots()
-plot = ax.contourf(yy, zz, vel_y_MFSim)
+plot = ax.contourf(yy, zz, vel_y)
 fig.colorbar(plot)
 
 fig, ax = plt.subplots()
-plot = ax.contourf(yy, zz, vel_z_MFSim)
+plot = ax.contourf(yy, zz, vel_z)
 fig.colorbar(plot)
 
-max_vel = y_vel_MFSim(0,0.0055)
-vel_mag = np.sqrt(vel_y_MFSim**2 + vel_z_MFSim**2)
+max_vel = y_vel_MFSim(0,r_jet)
+vel_mag = np.sqrt(vel_y**2 + vel_z**2)
 fig, ax = plt.subplots()
-strm = ax.streamplot(yy, zz, vel_y_MFSim, vel_z_MFSim,
+strm = ax.streamplot(yy, zz, vel_y, vel_z,
                     density=1,
                     color=vel_mag,
                     cmap='viridis',
@@ -67,34 +72,45 @@ ax.set_xlim(-r_jet,+r_jet)
 ax.set_ylim(-r_jet,+r_jet)
 fig.colorbar(strm.lines)
 
-dvy_dz = np.gradient(vel_y_MFSim, space, axis=0, edge_order=2)
-dvz_dy = np.gradient(vel_z_MFSim, space, axis=1, edge_order=2)
-vort = 0.5*(dvz_dy-dvy_dz)
+dvy_dz = np.gradient(vel_y, space, axis=0, edge_order=2)
+dvz_dy = np.gradient(vel_z, space, axis=1, edge_order=2)
+vort = (dvz_dy-dvy_dz)
 fig, ax = plt.subplots()
 plot = ax.contourf(yy, zz, vort)
 fig.colorbar(plot)
 
 # MFSim - y and z velocities without aux function
-vel_y_MFSim = y_vel_MFSim(yy,zz,1)
-vel_z_MFSim = z_vel_MFSim(yy,zz,1)
+vel_y = y_vel_MFSim(yy,zz,150000,1)
+vel_z = z_vel_MFSim(yy,zz,150000,1)
 
 fig, ax = plt.subplots()
-plot = ax.contourf(yy, zz, vel_y_MFSim)
+plot = ax.contourf(yy, zz, vel_y)
 fig.colorbar(plot)
 
 fig, ax = plt.subplots()
-plot = ax.contourf(yy, zz, vel_z_MFSim)
+plot = ax.contourf(yy, zz, vel_z)
 fig.colorbar(plot)
 
+max_vel = y_vel_MFSim(0,r_jet,150000,1)
+vel_mag = np.sqrt(vel_y**2 + vel_z**2)
 fig, ax = plt.subplots()
-strm = ax.streamplot(yy, zz, vel_y_MFSim, vel_z_MFSim,
+strm = ax.streamplot(yy, zz, vel_y, vel_z,
                     density=1,
-                    color=np.sqrt(vel_y_MFSim**2 + vel_z_MFSim**2),
-                    cmap='viridis'
+                    color=vel_mag,
+                    cmap='viridis',
+                    norm=colors.Normalize(vmin=0,
+                                          vmax=max_vel)
                     )
 ax.set_xlim(-r_jet,+r_jet)
 ax.set_ylim(-r_jet,+r_jet)
 fig.colorbar(strm.lines)
+
+dvy_dz = np.gradient(vel_y, space, axis=0, edge_order=2)
+dvz_dy = np.gradient(vel_z, space, axis=1, edge_order=2)
+vort = (dvz_dy-dvy_dz)
+fig, ax = plt.subplots()
+plot = ax.contourf(yy, zz, vort)
+fig.colorbar(plot)
 
 # TEST - y and z velocities with aux function
 vel_y = y_vel(yy,zz)
@@ -108,10 +124,10 @@ fig, ax = plt.subplots()
 plot = ax.contourf(yy, zz, vel_z)
 fig.colorbar(plot)
 
-max_vel = np.abs(y_vel(0,0.0055))
+max_vel = y_vel(0,r_jet)
 vel_mag = np.sqrt(vel_y**2 + vel_z**2)
 fig, ax = plt.subplots()
-strm = ax.streamplot(yy, zz, -vel_y, -vel_z,
+strm = ax.streamplot(yy, zz, vel_y, vel_z,
                     density=1,
                     color=vel_mag,
                     cmap='viridis',
@@ -122,16 +138,16 @@ ax.set_xlim(-r_jet,+r_jet)
 ax.set_ylim(-r_jet,+r_jet)
 fig.colorbar(strm.lines)
 
-dvy_dz = np.gradient(-vel_y, space, axis=0, edge_order=2)
-dvz_dy = np.gradient(-vel_z, space, axis=1, edge_order=2)
-vort = 0.5*(dvz_dy-dvy_dz)
+dvy_dz = np.gradient(vel_y, space, axis=0, edge_order=2)
+dvz_dy = np.gradient(vel_z, space, axis=1, edge_order=2)
+vort = (dvz_dy-dvy_dz)
 fig, ax = plt.subplots()
 plot = ax.contourf(yy, zz, vort)
 fig.colorbar(plot)
 
 # TEST - y and z velocities without aux function
-vel_y = y_vel(yy,zz,aux=1)
-vel_z = z_vel(yy,zz,aux=1)
+vel_y = y_vel(yy,zz,150000,1)
+vel_z = z_vel(yy,zz,150000,1)
 
 fig, ax = plt.subplots()
 plot = ax.contourf(yy, zz, vel_y)
@@ -141,12 +157,33 @@ fig, ax = plt.subplots()
 plot = ax.contourf(yy, zz, vel_z)
 fig.colorbar(plot)
 
+max_vel = y_vel(0,r_jet,150000,1)
+vel_mag = np.sqrt(vel_y**2 + vel_z**2)
 fig, ax = plt.subplots()
 strm = ax.streamplot(yy, zz, vel_y, vel_z,
                     density=1,
-                    color=np.sqrt(vel_y**2 + vel_z**2),
-                    cmap='viridis'
+                    color=vel_mag,
+                    cmap='viridis',
+                    norm=colors.Normalize(vmin=0,
+                                          vmax=max_vel)
                     )
 ax.set_xlim(-r_jet,+r_jet)
 ax.set_ylim(-r_jet,+r_jet)
 fig.colorbar(strm.lines)
+
+dvy_dz = np.gradient(vel_y, space, axis=0, edge_order=2)
+dvz_dy = np.gradient(vel_z, space, axis=1, edge_order=2)
+vort = (dvz_dy-dvy_dz)
+fig, ax = plt.subplots()
+plot = ax.contourf(yy, zz, vort)
+fig.colorbar(plot)
+
+vort = -3*2*np.pi*150000*np.sqrt(yy**2+zz**2)
+fig, ax = plt.subplots()
+plot = ax.contourf(yy, zz, vort)
+fig.colorbar(plot)
+
+vort = np.ones((len(space),len(space)))*(-2)*2*np.pi*150000
+fig, ax = plt.subplots()
+plot = ax.contourf(yy, zz, vort)
+fig.colorbar(plot)
